d9ddcbd2aa3519d869fbbd5d37932b7669edbe04
SrPhoenix <57529606+SrPhoenix@users.noreply.github.com>
2021-04-17 14:30:35 +0100

    Update ATL.sql
    acrescentei alguns checks e identity's e resolvi algumas faltas de atenção

--- SQLS/ATL.sql
+++ SQLS/ATL.sql
@@ -1,5 +1,5 @@
 CREATE TABLE ATL_Pessoa(
-    N_CC                      INT                     NOT NULL                                              PRIMARY KEY,
+    N_CC                      INT                     NOT NULL           IDENTITY(30000000,1)                                   PRIMARY KEY,
     Data_nascimento           DATETIME                NOT NULL, 
     Morada                    VARCHAR(1024)           NOT NULL, 
     Telefone                  VARCHAR(15)             NOT NULL, 
@@ -10,7 +10,7 @@ CREATE TABLE ATL_Pessoa(
 GO
 
 CREATE TABLE ATL_Encarregado_edu(
-    P_N_CC                    INT                     NOT NULL            PRIMARY KEY CONSTRAINT FK_ATL_Encarregado_edu_P_N_CC           FOREIGN KEY (P_N_CC)                      REFERENCES ATL_Pessoa(N_CC),
+    P_N_CC                    INT                     NOT NULL           PRIMARY KEY CONSTRAINT FK_ATL_Encarregado_edu_P_N_CC           FOREIGN KEY (P_N_CC)                      REFERENCES ATL_Pessoa(N_CC),
     Data_nascimento           DATETIME                NOT NULL, 
     Morada                    VARCHAR(1024)           NOT NULL, 
     Telefone                  VARCHAR(15)             NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE ATL_Encarregado_edu(
 GO
 
 CREATE TABLE ATL_Professor(
-    No_funcionario            INT                     NOT NULL                                              PRIMARY KEY,
+    No_funcionario            INT                     NOT NULL           IDENTITY(1,1)											PRIMARY KEY,
     Nome                      VARCHAR(256)            NOT NULL, 
     Telefone                  VARCHAR(15)             NOT NULL, 
     Data_nascimento           DATETIME                NOT NULL, 
@@ -32,19 +32,20 @@ CREATE TABLE ATL_Professor(
 GO
 
 CREATE TABLE ATL_Turma(
-    Identificador             INT                     NOT NULL                                              PRIMARY KEY, 
-    Prof_no_funcionario       INT                     NOT NULL            CONSTRAINT FK_ATL_Turma_Prof_no_funcionario                   FOREIGN KEY (Prof_no_funcionario)          REFERENCES ATL_Professor(No_funcionario), 
+    Identificador             INT                     NOT NULL           IDENTITY(1,1)                                   PRIMARY KEY, 
+    Prof_no_funcionario       INT                     NOT NULL           CONSTRAINT FK_ATL_Turma_Prof_no_funcionario                   FOREIGN KEY (Prof_no_funcionario)          REFERENCES ATL_Professor(No_funcionario), 
     N_max_Aluno               INT                     NOT NULL, 
     Ano_letivo                VARCHAR(30)             NOT NULL, 
     Classes                   VARCHAR(15)             NOT NULL,              
-    Designacao                VARCHAR(1024)           NOT NULL
+    Designacao                VARCHAR(1024)           NOT NULL,
+	CHECK(N_max_Aluno > 0)
 );
 GO
 
 CREATE TABLE ATL_Aluno(
-    N_CC                      INT                     NOT NULL                                              PRIMARY KEY,
+    N_CC                      INT                     NOT NULL           IDENTITY(30000000,1)                                  PRIMARY KEY,
     Turma_ID                  INT                     NOT NULL           CONSTRAINT FK_ATL_Aluno_Turma_ID                               FOREIGN KEY (Turma_ID)                     REFERENCES ATL_Turma(Identificador), 
-    E_N_CC                    INT                     NOT NULL           CONSTRAINT FK_ATL_Aluno_E_N_CC                                 FOREIGN KEY (E_N_CC)                       REFERENCES ATL_Encarregado_edu(P_N_CC)), 
+    E_N_CC                    INT                     NOT NULL           CONSTRAINT FK_ATL_Aluno_E_N_CC                                 FOREIGN KEY (E_N_CC)                       REFERENCES ATL_Encarregado_edu(P_N_CC), 
     Data_nascimento           DATETIME                NOT NULL, 
     Morada                    VARCHAR(1024)           NOT NULL, 
     Nome                      VARCHAR(256)            NOT NULL, 
@@ -52,37 +53,40 @@ CREATE TABLE ATL_Aluno(
 );
 GO
 
-CREATE ATL_Entrega_ou_levanta(
+CREATE TABLE ATL_Entrega_ou_levanta(
     P_N_CC                    INT                     NOT NULL           CONSTRAINT FK_ATL_Entrega_ou_levanta_P_N_CC                    FOREIGN KEY (P_N_CC)                       REFERENCES ATL_Encarregado_edu(P_N_CC),
     A_N_CC                    INT                     NOT NULL           CONSTRAINT FK_ATL_Entrega_ou_levanta_A_N_CC                    FOREIGN KEY (A_N_CC)                       REFERENCES ATL_Aluno(N_CC),
     PRIMARY KEY (P_N_CC, A_N_CC)
 );
 GO
 
-CREATE ATL_Processos_fin(
+CREATE TABLE ATL_Processos_fin(
     A_N_CC                    INT                     NOT NULL            PRIMARY KEY CONSTRAINT FK_ATL_Processos_fin_A_N_CC             FOREIGN KEY (A_N_CC)                      REFERENCES ATL_Aluno(N_CC),
     Desconto_familia          VARCHAR(15)             NOT NULL, 
     Pagamentos                INT                     DEFAULT 0, 
     Mensalidades              INT                     DEFAULT 0, 
-    Atividades                VARCHAR(256)            NOT NULL
+    Atividades                VARCHAR(256)            NOT NULL,
+	CHECK (pagamentos >= 0),
+	CHECK (Mensalidades >= 0)
 );
 GO
 
-CREATE ATL_Atividade(
-    Identificador            INT                      NOT NULL                                              PRIMARY KEY,
+CREATE TABLE ATL_Atividade(
+    Identificador            INT                      NOT NULL				IDENTITY(1,1)                                            PRIMARY KEY,
     Designacao               VARCHAR(1024)            NOT NULL, 
-    Custo                    INT                      DEFAULT 0
+    Custo                    INT                      DEFAULT 0,
+	CHECK(custo >= 0)
 );
 GO
 
-CREATE ATL_Pratica(
+CREATE TABLE ATL_Pratica(
     Turma_ID                  INT                     NOT NULL           CONSTRAINT FK_ATL_Pratica_Turma_ID                             FOREIGN KEY (Turma_ID)                     REFERENCES ATL_Turma(Identificador),
     Ativ_ID                   INT                     NOT NULL           CONSTRAINT FK_ATL_Pratica_Ativ_ID                              FOREIGN KEY (Ativ_ID)                      REFERENCES ATL_Atividade(Identificador),
     PRIMARY KEY (Turma_ID, Ativ_ID)
 );
 GO
 
-CREATE ATL_Deriva(
+CREATE TABLE ATL_Deriva(
     A_Identificador           INT                     NOT NULL           CONSTRAINT FK_ATL_Deriva_A_Identificador                       FOREIGN KEY (A_Identificador)              REFERENCES ATL_Atividade(Identificador),
     A_N_CC                    INT                     NOT NULL           CONSTRAINT FK_ATL_Deriva_A_N_CC                                FOREIGN KEY (A_N_CC)                       REFERENCES ATL_Processos_fin(A_N_CC),
     PRIMARY KEY (A_Identificador, A_N_CC)
